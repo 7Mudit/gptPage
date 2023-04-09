58a4d53b8783a2012b486970b23b17dae886597f
Mudit Kapoor <muditk441@gmail.com>
2023-04-10 00:25:50 +0530

    hero section made

--- src/components/brand/Brand.js
+++ src/components/brand/Brand.js
@@ -1,9 +1,29 @@
-import React from 'react'
-import './Brand.css'
-const Brand = () => {
-  return (
-    <div>Brand</div>
-  )
-}
+import React from 'react';
+import './Brand.css';
+import google from '../../assets/google.png'
+import slack from '../../assets/slack.png'
+import atlassian from '../../assets/atlassian.png'
+import dropbox from '../../assets/dropbox.png'
+import shopify from '../../assets/shopify.png'
 
-export default Brand
+const Brand = () => (
+  <div className="gpt3__brand section__padding">
+    <div>
+      <img src={google} alt='Not Found' />
+    </div>
+    <div>
+      <img src={slack}  alt='Not Found' />
+    </div>
+    <div>
+      <img src={atlassian} alt='Not Found'  />
+    </div>
+    <div>
+      <img src={dropbox} alt='Not Found'  />
+    </div>
+    <div>
+      <img src={shopify}  alt='Not Found' />
+    </div>
+  </div>
+);
+
+export default Brand;
